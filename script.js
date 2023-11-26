5b1c58546a9fd3fea3f4ec9fb08e22ea178767a8
Anton Sjöstrand <asjoa99@hotmail.com>
2023-11-26 11:45:47 +0100

    Gjort en massa grejer

--- script.js
+++ script.js
@@ -63,3 +63,77 @@ function myFunction() {
       behavior: "smooth"
     });
   }
+
+
+  function addToCart(button) {
+    // Sparar den ursprungliga knappens text
+    var originalText = button.textContent;
+
+    // Uppdaterar knappens text och inaktiverar knappen
+    button.textContent = "Tillagd";
+    button.disabled = true;
+
+    // Återställer knappens text och aktiverar knappen efter 3 sekunder (justera vid behov)
+    setTimeout(function () {
+      button.textContent = originalText;
+      button.disabled = false;
+    }, 2000); // 3000 millisekunder = 3 sekunder
+  }
+
+
+
+/* const form = document.getElementById('contactForm');
+const phoneInput = document.getElementById('phone');
+const phoneWarning = document.getElementById('phoneWarning');
+
+form.addEventListener('submit', function (event) {
+  if (!isValidPhone(phoneInput.value)) {
+    phoneWarning.textContent = 'Ogiltigt telefonnummer. Använd endast siffror.';
+    event.preventDefault();
+  } else {
+    phoneWarning.textContent = '';
+  }
+});
+
+window.addEventListener('beforeunload', function (event) {
+  event.returnValue = 'Vill du verkligen lämna sidan? Dina ändringar kommer att förloras.';
+});
+
+function isValidPhone(phone) {
+  const phonePattern = /^\d*$/; // Endast siffror, inget specifikt format
+  return phonePattern.test(phone);
+}
+ */
+
+
+
+
+const form = document.getElementById('contactForm');
+const confirmationMessage = document.querySelector('.confirmation-message');
+
+// Lägg till eventlyssnare för formulärsubmit
+form.addEventListener('submit', function (event) {
+    // Visa bekräftelsemeddelande
+    confirmationMessage.style.display = 'block';
+
+    // Förhindra formuläret från att skickas till servern och uppdatera sidan
+    event.preventDefault();
+
+    // Dölj bekräftelsemeddelandet efter 3 sekunder (justera vid behov)
+    setTimeout(function () {
+        confirmationMessage.style.display = 'none';
+    }, 3000);
+});
+
+// Lägg till eventlyssnare för försök att lämna sidan
+window.addEventListener('beforeunload', function (event) {
+    // Kontrollera om formuläret är oskickat
+    if (confirmationMessage.style.display === 'block') {
+        return null;
+    }
+
+    // Om formuläret inte har skickats, visa varningsmeddelande
+    const message = 'Vill du verkligen lämna sidan? Dina ändringar kommer att förloras.';
+    event.returnValue = message;
+    return message;
+});
